5d7d732e4623791e1f5347b3df63003ce2ba7ad9
陈伟 <chen_jason@outlook.com>
2026-01-22 11:50:54 +0800

    recursion ReverseString

---
+++ CPPAlgorithm/recursion/LinearRecursion/04_ReverseString.cpp
@@ -0,0 +1,22 @@
+﻿// recursion ReverseString  -- Single-Branch Linear Recursion
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Print string in reverse
+void reverseString(string str, int index = 0) {
+    // Base case: reached end of string
+    if (index == str.length()) {
+        return;
+    }
+
+    // Recursive case: ONE recursive call
+    reverseString(str, index + 1);
+    cout << str[index];
+}
+
+int main() {
+    reverseString("Hello");  // Output: olleH
+    cout << endl;
+    return 0;
+}
