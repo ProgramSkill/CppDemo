64e657cb16d606a5ae41088c93c3d1a9b456d8cd
陈伟 <chen_jason@outlook.com>
2026-01-22 12:01:30 +0800

    Greatest Common Divisor -- Single-Branch Linear Recursion

---
+++ CPPAlgorithm/recursion/LinearRecursion/06_GreatestCommonDivisor.cpp
@@ -0,0 +1,19 @@
+﻿//Greatest Common Divisor -- Single-Branch Linear Recursion
+#include <iostream>
+using namespace std;
+
+// Find GCD using Euclidean algorithm
+int gcd(int a, int b) {
+    // Base case: when b is 0, a is the GCD
+    if (b == 0) {
+        return a;
+    }
+
+    // Recursive case: ONE recursive call
+    return gcd(b, a % b);
+}
+
+int main() {
+    cout << "GCD(48, 18) = " << gcd(48, 18) << endl;  // Output: 6
+    return 0;
+}
